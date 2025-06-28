e783a8dee0620bbce809b7ec749f300b481dc964
Naif Hagatulah <90781179+NaifHagatulah@users.noreply.github.com>
2025-06-29 03:44:56 +0800

    fixing projectile logic

--- SpaceInvadersGL/SpaceInvadersGL/include/ProjectileUpdateProvider.hpp
+++ SpaceInvadersGL/SpaceInvadersGL/include/ProjectileUpdateProvider.hpp
@@ -1,12 +1,21 @@
+﻿// ProjectileUpdateProvider.hpp
 #pragma once
 #include "IUpdateProvider.hpp"
 #include "Projectile.hpp"
+#include <SFML/Graphics.hpp>
 
 class ProjectileUpdateProvider : public IUpdateProvider {
 public:
+    ProjectileUpdateProvider(const sf::FloatRect& bounds)
+        : m_bounds(bounds)   
+    {
+    }
+
     void update(GameObject& baseObj, float dt) override {
         auto& p = static_cast<Projectile&>(baseObj);
         p.getSprite().move(p.getVelocity() * dt);
         if (p.isOutOfBounds()) p.kill();
     }
+private:
+    sf::FloatRect m_bounds;   
 };
